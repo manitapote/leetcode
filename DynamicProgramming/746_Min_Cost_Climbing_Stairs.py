e3edaa91fd74c0e7476fd8b8b77746bc916e0882
manitapote <manitapote@gmail.com>
2025-02-08 22:14:12 -0500

    E_746

---
+++ DynamicProgramming/746_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,27 @@
+# # 746 Min Cost Climbing Stairs
+#
+# # You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+# You can either start from the step with index 0, or the step with index 1.
+#
+# Return the minimum cost to reach the top of the floor.
+
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        # cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1, 0]
+        cost.append(0)
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] = cost[i] + min(cost[i+1], cost[i+2])
+
+        return min(cost[0], cost[1])
+
+
+
+
+
+cost = [10, 15, 20]
+a = Solution()
+print(a.minCostClimbingStairs(cost))
+
+cost = [1,100,1,1,1,100,1,1,100,1]
+a = Solution()
+print(a.minCostClimbingStairs(cost))
