bf6437820ea32d46117fb4ef2613bfb859b12372
manitapote <manitapote@gmail.com>
2025-03-20 18:22:59 -0400

    pattern_graph_bipartite

--- graphs/bipartite_graph_validation.py
+++ graphs/bipartite_graph_validation.py
@@ -1,5 +1,8 @@
 from typing import List
 def bipartite_graph_validation(graph: List[List[int]]):
+    #T= O(n)
+    #T dfs = O(n+e)
+    #S = O(n)
     colors = [0]*len(graph)
     for i in range(len(graph)):
         if colors[i] == 0 and dfs(i, graph, 1, colors) == False:
