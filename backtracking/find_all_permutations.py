a0f781fdfa0800c4662a38b46db9189afd446bd2
manitapote <manitapote@gmail.com>
2025-03-22 15:53:33 -0400

    time complexity added

--- backtracking/find_all_permutations.py
+++ backtracking/find_all_permutations.py
@@ -4,16 +4,20 @@ def all_permutations(nums: List[int]) -> List:
 
 def dfs(result, nums, chosen):
     if len(chosen) == len(nums):
+        #O(n)
         result.append(chosen[:])
 
         return
 
+    #O(n)
     for x in nums:
+        #O(n)
         if x in chosen:
             continue
 
         chosen.append(x)
 
+        #T=O(n!)
         dfs(result, nums, chosen)
 
         chosen.remove(x)
@@ -23,3 +27,5 @@ def dfs(result, nums, chosen):
 
 nums = [4, 5, 6]
 print(all_permutations(nums))
+#T= O(n!)*(O(n)*O(n) + O(n))
+#T=O(n^2. n!)
