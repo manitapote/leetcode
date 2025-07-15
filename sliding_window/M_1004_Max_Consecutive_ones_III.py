3736f56d3d243f1054dea341e94b3af791a791ee
manitapote <manitapote@gmail.com>
2025-07-15 17:29:47 -0400

    M_1004

--- sliding_window/M_1004_Max_Consecutive_ones_III.py
+++ sliding_window/M_1004_Max_Consecutive_ones_III.py
@@ -5,21 +5,21 @@ class Solution:
                     nums: List[int],
                     k: int
                     ) -> int:
-        hashmap = defaultdict(int)
-        left = 0
-        l = 1
-        total = 0
-        for i, val in enumerate(nums):
-            # while l < k and val == 0:
-            #     total = max(total, i - left + 1)
-            #     l += 1
-            # left = i+1
-            #
-            # if l >= k:
-            #     l = 0
+        l = 0
+        total0 = 0
+        ans = float('-inf')
+        for r, val in enumerate(nums):
+            if val == 0:
+                total0 += 1
 
+            while total0 > k:
+                if nums[l] == 0:
+                    total0 -= 1
+                l += 1
 
-        return total
+            ans = max(ans, r - l + 1)
+
+        return ans
 
 nums = [1,1,1,0,0,0,1,1,1,1,0]
 k = 2
